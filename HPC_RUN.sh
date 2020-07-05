6da1b1f81deff2d03bfbf1f47e44f2bb7d038693
Olmo Zavala-Romero <olmozavala@gmail.com>
2020-07-05 11:57:39 -0400

    Fixed HPC_RUN and added HPC_Merge

--- HPC_RUN.sh
+++ HPC_RUN.sh
@@ -14,34 +14,51 @@ module load anaconda3.7.3
 start_date_str="2010-01-01"
 end_date_str="2015-01-01"
 output_path="/gpfs/home/osz09/scratch/output/"
-run_name="NoWinds_YesDiffusion_2010_01"
+run_name="YesWinds_NoDiffusion_2010_01"
 inc_per_run=30
 
 t=0
 c_start_date=$(date --date="${start_date_str} +$((t)) days" "+%Y-%m-%d")
-c_end_date=$(date --date="${start_date_str} +$((t+inc_per_run)) days" "+%Y-%m-%d")
 end_date=$(date --date="${end_date_str}")
 
 c_start_date_sec=$(date --date="${start_date_str} +$((t)) days" "+%s")
 end_date_sec=$(date --date="${end_date_str}" "+%s")
+
+
+c_end_date_sec=$(date --date="${start_date_str} +$((t+inc_per_run)) days" "+%s")
+if [ $c_end_date_sec -lt $end_date_sec ]
+then
+  c_end_date=$(date --date="${start_date_str} +$((t+inc_per_run)) days" "+%Y-%m-%d")
+else
+  c_end_date=$(date --date="${end_date_str}" "+%Y-%m-%d")
+fi
+
 
 while [ $c_start_date_sec -lt $end_date_sec ]
 do
   echo "====================== NEW RUN t=$t ================================"
   if [ $t -eq  0 ]
   then
-    cmd="srun /gpfs/home/osz09/.conda/envs/py3_parcels_mpi/bin/python 0_WorldLitter.py ${c_start_date}:0 ${c_end_date}:0 False True False  ${run_name}_${c_start_date}_${c_end_date}"
+    #cmd="srun /gpfs/home/osz09/.conda/envs/py3_parcels_mpi/bin/python 0_WorldLitter.py ${c_start_date}:0 ${c_end_date}:0 False True False  ${run_name}_${c_start_date}_${c_end_date}"
+    cmd="srun /gpfs/home/osz09/.conda/envs/py3_parcels_mpi/bin/python 0_WorldLitter.py ${c_start_date}:0 ${c_end_date}:0 True False False  ${run_name}_${c_start_date}_${c_end_date}"
   else
-    cmd="srun /gpfs/home/osz09/.conda/envs/py3_parcels_mpi/bin/python 0_WorldLitter.py ${c_start_date}:0 ${c_end_date}:0 False True False $run_name $output_path/${run_name}_${prev_start_date}_${prev_end_date}.nc $inc_per_run"
+    #cmd="srun /gpfs/home/osz09/.conda/envs/py3_parcels_mpi/bin/python 0_WorldLitter.py ${c_start_date}:0 ${c_end_date}:0 False True False $run_name $output_path/${run_name}_${prev_start_date}_${prev_end_date}.nc $inc_per_run"
+    cmd="srun /gpfs/home/osz09/.conda/envs/py3_parcels_mpi/bin/python 0_WorldLitter.py ${c_start_date}:0 ${c_end_date}:0 True False False $run_name $output_path/${run_name}_${prev_start_date}_${prev_end_date}.nc $inc_per_run"
   fi
   echo $cmd
-  `$cmd > "${run_name}.log"`
+  #`$cmd > "${run_name}.log"`
   t=$[$t+$inc_per_run]
   prev_start_date=$c_start_date
   prev_end_date=$c_end_date
   c_start_date_sec=$(date --date="${start_date_str} +$((t)) days" "+%s")
   c_start_date=$(date --date="${start_date_str} +$((t)) days" "+%Y-%m-%d")
-  c_end_date=$(date --date="${start_date_str} +$((t+inc_per_run)) days" "+%Y-%m-%d")
+  c_end_date_sec=$(date --date="${start_date_str} +$((t+inc_per_run)) days" "+%s")
+  if [ $c_end_date_sec -lt $end_date_sec ]
+  then
+      c_end_date=$(date --date="${start_date_str} +$((t+inc_per_run)) days" "+%Y-%m-%d")
+  else
+      c_end_date=$(date --date="${end_date_str}" "+%Y-%m-%d")
+  fi
 done
 
 
